54d22d96d9a5aab8a09e1d90abb742702cd7c0ee
Kjartan Olason <olasonk@gmail.com>
2020-09-12 10:34:34 -0300

    Security Configuration

---
+++ src/main/java/com/github/kjarrio/store/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.github.kjarrio.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .logout().permitAll().and()
+            .formLogin().permitAll().and()
+            .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers("/logout").permitAll()
+                .antMatchers(HttpMethod.GET, "/products/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/products/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/users/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/purchases/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/purchases/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/purchases/**").hasRole("ADMIN")
+                .anyRequest().permitAll();
+
+
+    }
+
+}
